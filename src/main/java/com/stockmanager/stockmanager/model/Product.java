ce979564d89f0eb7158a2daf0612bd15d0d22221
theTrickster123 <tarissi.mehdi2019@gmail.com>
2025-04-10 03:16:25 +0100

    Updated my Product Entity

--- src/main/java/com/stockmanager/stockmanager/model/Product.java
+++ src/main/java/com/stockmanager/stockmanager/model/Product.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Objects;
 
 @Entity
 @Table(name="product")
@@ -58,9 +60,9 @@ public class Product {
     @Column(nullable = true)
     private byte[] image;
 
-    // Getters and Setters
 
-    @PrePersist
+    //To configure correctly
+    /*@PrePersist
     protected void onCreate() {
         createdAt = LocalDateTime.now();
         updatedAt = LocalDateTime.now();
@@ -69,7 +71,176 @@ public class Product {
     @PreUpdate
     protected void onUpdate() {
         updatedAt = LocalDateTime.now();
+    }*/
+
+    public Product(Long id, String title, String description, BigDecimal price, Integer quantity, Category category, LocalDateTime createdAt, LocalDateTime updatedAt, Boolean isActive, String manufacturer, Boolean isOutOfStock, String details, BigDecimal totalIncome, BigDecimal totalCharges, byte[] image) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.isActive = isActive;
+        this.manufacturer = manufacturer;
+        this.isOutOfStock = isOutOfStock;
+        this.details = details;
+        this.totalIncome = totalIncome;
+        this.totalCharges = totalCharges;
+        this.image = image;
     }
 
+    public Product() {
 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Boolean getOutOfStock() {
+        return isOutOfStock;
+    }
+
+    public void setOutOfStock(Boolean outOfStock) {
+        isOutOfStock = outOfStock;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public BigDecimal getTotalIncome() {
+        return totalIncome;
+    }
+
+    public void setTotalIncome(BigDecimal totalIncome) {
+        this.totalIncome = totalIncome;
+    }
+
+    public BigDecimal getTotalCharges() {
+        return totalCharges;
+    }
+
+    public void setTotalCharges(BigDecimal totalCharges) {
+        this.totalCharges = totalCharges;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", category=" + category +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                ", isActive=" + isActive +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", isOutOfStock=" + isOutOfStock +
+                ", details='" + details + '\'' +
+                ", totalIncome=" + totalIncome +
+                ", totalCharges=" + totalCharges +
+                ", image=" + Arrays.toString(image) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) && Objects.equals(title, product.title) && Objects.equals(description, product.description) && Objects.equals(price, product.price) && Objects.equals(quantity, product.quantity) && Objects.equals(category, product.category) && Objects.equals(createdAt, product.createdAt) && Objects.equals(updatedAt, product.updatedAt) && Objects.equals(isActive, product.isActive) && Objects.equals(manufacturer, product.manufacturer) && Objects.equals(isOutOfStock, product.isOutOfStock) && Objects.equals(details, product.details) && Objects.equals(totalIncome, product.totalIncome) && Objects.equals(totalCharges, product.totalCharges) && Objects.deepEquals(image, product.image);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, price, quantity, category, createdAt, updatedAt, isActive, manufacturer, isOutOfStock, details, totalIncome, totalCharges, Arrays.hashCode(image));
+    }
 }
